3a29ab8f6c68b88aa7291f51f57a8c85d435a600
iamvishal345 <vishalagnihotri345@gmail.com>
2021-04-05 23:01:33 +0530

    Added some new solutions

--- largestSum.js
+++ largestSum.js
@@ -2,11 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    let sum ={}
-    for (let i = 0; i < nums.length; i++) {
-        
-    }
+var maxSubArray = function (nums) {
+  let max = nums[0];
+  let prev = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    prev = Math.max(prev + nums[i], nums[i]);
+    max = Math.max(prev, max);
+  }
+  return max;
 };
-
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
--- twoSum.js
+++ twoSum.js
@@ -3,15 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let diff = {};
-    for (let i = 0; i < nums.length; i++) {
-        if(diff[nums[i]]>=0){
-            return [diff[nums[i]],i]
-        }else{
-            diff[target-nums[i]] = i
-        }
+var twoSum = function (nums, target) {
+  let diff = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (diff[nums[i]] >= 0) {
+      return [diff[nums[i]], i];
+    } else {
+      diff[target - nums[i]] = i;
     }
+  }
 };
 
-console.log(twoSum([2, 7, 11, 15],9));
+console.log(twoSum([2, 7, 11, 15], 9));
+
+const factObj = {};
+const factorial = (num) => {
+  if (num === 2) return 2;
+  if (factObj[num]) {
+    return factObj[num];
+  } else {
+    factObj[num] = num * factorial(num - 1);
+  }
+  return factObj[num];
+};
+
+console.log(factorial(5));
